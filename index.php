d14472adbd9a00624f44c254dec81500d7588c2d
Guy Marshall <guy@cslgroupservices.co.uk>
2022-04-26 12:36:57 +0100

    Add basic table for files + comments

--- index.php
+++ index.php
@@ -13,3 +13,37 @@
     </form>
 </body>
 </html>
+
+<?php
+    // get files list
+    $files = [
+        0 => [
+            'filename' => 'tree',
+            'filetype' => '.jpg',
+            'filesize' => '243KB'
+        ],
+        1 => [
+            'filename' => 'backup',
+            'filetype' => '.zip',
+            'filesize' => '24MB'
+        ]
+    ];
+    // iterate through and generate a table, one row for each file
+    $table_columns = ['filename', 'filetype', 'filesize'];
+?>
+<table style="border: 1px solid black; border-collapse: collapse;">
+    <tr>
+        <th>Filename</th>
+        <th>Filetype</th>
+        <th>Filesize</th>
+    </tr>
+    <?php if (isset($files)): ?>
+        <?php foreach ($files as $file): ?>
+        <tr>
+            <td><?php echo $file['filename']; ?></td>
+            <td><?php echo $file['filetype']; ?></td>
+            <td><?php echo $file['filesize']; ?></td>
+        </tr>
+        <?php endforeach; ?>
+    <?php endif; ?>
+</table>
