e4c887b8a8ccb9609636609a02cb1c251e5b78fb
Guy Marshall <guymarshall9@gmail.com>
2022-05-25 20:40:41 +0100

    Index page now shows files

--- pages/index.php
+++ pages/index.php
@@ -4,10 +4,31 @@ require 'page.php';
 
 $content = '';
 
+// without . and ..
+$files = array_diff(scandir('../uploads'), ['..', '.']);
+$table_columns = ['Filename', 'Filetype'];
+
 $content .= '
-<p>
-    FILES SHOULD APPEAR HERE
-</p>';
+<table style="border: 1px solid black; border-collapse: collapse;">
+    <tr>';
+    foreach ($table_columns as $column) {
+        $content .= '<th>'.$column.'</th>';
+    }
+    $content .= '
+    </tr>';
+    if (isset($files)) {
+        foreach ($files as $file) {
+            $content .= '
+                <tr>
+                    <td>'.$file.'</td>
+                    <td>'.pathinfo($file, PATHINFO_EXTENSION).'</td>
+                </tr>
+            ';
+        }
+    }
+    $content .= '
+</table>
+';
 
 $o_index_page = new Page();
 $index_html = $o_index_page->create_page('Files', $content);
