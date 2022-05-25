ca948eabf4023b5dbcc8d635a9d5490cfdf44d65
Guy Marshall <guymarshall9@gmail.com>
2022-05-25 20:27:31 +0100

    Add and implement upload.php

--- pages/upload.php
+++ pages/upload.php
@@ -1,37 +1,16 @@
 <?php
 
-require "defines.php";
+require 'page.php';
 
-if (isset($_POST['submit'])) {
-    $file = $_FILES['file'];
+$content = '';
 
-    $file_name = $file['name'];
-    $file_temporary_name = $file['tmp_name'];
-    $file_size = $file['size'];
-    $file_error = $file['error'];
-    $file_type = $file['type'];
+$content .= '
+<form action="upload_controller.php" method="post" enctype="multipart/form-data">
+  <input type="file" name="file" id="file">
+  <button type="submit" name="submit" id="submit">Upload</button>
+</form>';
 
-    $file_extension = explode('.', $file_name);
-    $file_actual_extension = strtolower(end($file_extension));
 
-    $file_size_limit = 500*KB;
-
-    $allowed_extensions = ['jpg', 'jpeg', 'png'];
-
-    if (in_array($file_actual_extension, $allowed_extensions)) {
-        if ($file_error === 0) {
-            if ($file_size < $file_size_limit) {
-                $file_name_new = uniqid('', true).'.'.$file_actual_extension;
-                $file_destination = 'uploads/'.$file_name_new;
-                move_uploaded_file($file_temporary_name, $file_destination);
-                header('Location: index.php?uploadsuccess');
-            } else {
-                echo 'Your file is larger than '.$file_size_limit.'bytes!';
-            }
-        } else {
-            echo 'There was an error uploading your file!';
-        }
-    } else {
-        echo 'You cannot upload '.$file_actual_extension.' files';
-    }
-}
+$o_upload_page = new Page();
+$upload_html = $o_upload_page->create_page('Upload', $content);
+echo $upload_html;
