b7173604fc7913d8714cba681a29f34a3039ba41
Guy Marshall <guymarshall9@gmail.com>
2022-05-25 20:04:51 +0100

    Add capital hint to add files

--- pages/index.php
+++ pages/index.php
@@ -6,10 +6,7 @@ $content = '';
 
 $content .= '
 <p>
-    This is my website! It includes projects I have completed. Feel free to have a look around!
-</p>
-<p>
-    If you have any suggestions for projects or change requests, visit my <a href="https://github.com/guymarshall">GitHub</a>, or <a href="mailto:guymarshall.c@gmail.com">email</a> me.
+    FILES SHOULD APPEAR HERE
 </p>';
 
 $o_index_page = new Page();
